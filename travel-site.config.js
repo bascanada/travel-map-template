1cf92b358c989477eeafde300da8702b97590c09
William Quintal <william95quintalwilliam@outlook.com>
2025-07-28 03:18:52 -0400

    relative url

--- travel-site.config.js
+++ travel-site.config.js
@@ -11,9 +11,9 @@
 const config = {
 	title: 'William Quintal Travel Site',
 	description: 'Share your travel adventures',
-	logo: '/favicon.svg',
-	favicon: '/favicon.svg',
-	travelIndex: '/data/index.json',
+	logo: 'favicon.svg',
+	favicon: 'favicon.svg',
+	travelIndex: 'data/index.json',
 	skeletonTheme: 'seafoam'
 };
 
